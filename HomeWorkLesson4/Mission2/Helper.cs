91205023d2c3e9746165412a4f68271072e28d7a
enemy <nkalmykov937@gmail.com>
2019-02-05 21:52:25 +0300

    Выполнил задание номер 2: а) Дописать класс для работы с одномерным массивом. Реализовать конструктор, создающий массив заданной размерности и заполняющий массив числами от начального значения с заданным шагом. Создать свойство Sum, которые возвращают сумму элементов массива, метод Inverse, меняющий знаки у всех элементов массива, метод Multi, умножающий каждый элемент массива на определенное число, свойство MaxCount, возвращающее количество максимальных элементов. В Main продемонстрировать работу класса.

---
+++ HomeWorkLesson4/Mission2/Helper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    class Helper
+    {
+        public int[] MainArray;
+
+        public Helper(int count, int start_value, int step)
+        {
+            MainArray= new int[count];
+            MainArray[0] = start_value;
+            int i;
+            for (i = 1; i < count; i++)
+            {
+                MainArray[i] = start_value + step;
+                start_value += step;
+            }
+
+            
+
+        }
+
+        public static int Sum(int[] mass)
+        {
+            int sum = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                sum = sum + mass[i];
+            }  
+
+            return sum;
+        }
+
+        /// <summary>
+        /// программа, изменяющая порядок элеметов в массиве
+        /// </summary>
+        /// <param name="mass"></param>
+        /// <returns></returns>
+        public static Array Inverse(Array mass)
+        {
+
+            Array.Reverse(mass);
+
+            return mass;
+        }
+
+        public static int Multi(int[] mass)
+        {
+            int Multi = 1;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Multi = Multi * mass[i];
+            }
+
+            return Multi;
+        }
+
+        public static int MaxCount(int[] mass)
+        {
+
+            int maxValue = mass.Max();
+
+
+            return maxValue;
+        }
+    }
+}
